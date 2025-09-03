1003b69e9a0e935cdb5432dc5250d8e63dd176a7
AnahAntonerrie <andrianahanah31@gmail.com>
2025-09-03 12:40:35 +0300

    code

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from typing import List, Dict
 app = FastAPI()
+class Characteristic(BaseModel):
+    max_speed: inta
+    max_fuel_capacity: int
 
-@app.get("/ping")
-def ping():
-    return {"message":"pong"}
+class Car(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+BDD: Dict[str, Car] = {}
+@app.get("/cars", response_model=List[Car])
+def get_all_cars():
+    """
+    Récupère la liste de toutes les voitures stockées en mémoire.
+    """
+    return list(BDD.values())
